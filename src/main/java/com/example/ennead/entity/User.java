31c0dbc3e0e42c9f097631be2bce2bd56b6b30b6
Minji-Sohn2 <cyzh1311@gmail.com>
2023-06-30 14:41:39 +0900

    회원가입 구현 SignupRequestDto, Timestamped 생성

--- src/main/java/com/example/ennead/entity/User.java
+++ src/main/java/com/example/ennead/entity/User.java
@@ -1,4 +1,32 @@
 package com.example.ennead.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String username, String nickname, String password) {
+        this.username = username;
+        this.nickname = nickname;
+        this.password = password;
+    }
 }
