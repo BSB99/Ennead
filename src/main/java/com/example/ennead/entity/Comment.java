ee450a4a4e03c0e1ea50c85eb5ae34645f0337c3
SeungBum Baek <wer06099@naver.com>
2023-06-30 12:10:57 +0900

    댓글 테이블 생성

--- src/main/java/com/example/ennead/entity/Comment.java
+++ src/main/java/com/example/ennead/entity/Comment.java
@@ -1,4 +1,25 @@
 package com.example.ennead.entity;
 
+import com.example.ennead.dto.CommentRequestDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "comments")
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    public Comment(CommentRequestDto request) {
+        this.content = request.getContent();
+    }
 }
