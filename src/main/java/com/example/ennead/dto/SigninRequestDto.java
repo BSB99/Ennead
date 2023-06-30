e58c6aa669f4b285642ffa1c87e89a9b55d6a4dd
Minji-Sohn2 <cyzh1311@gmail.com>
2023-06-30 18:06:08 +0900

    spring security jwt로 로그인 구현 (build.gradle에 jwt 추가), /api/auth/** 제외한 주소는 모두 인증 필요하도록 설정, User가 Timestamped 상속받도록 수정 UserService signup 중복된 회원 찾는 부분 람다식으로 수정

---
+++ src/main/java/com/example/ennead/dto/SigninRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.ennead.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SigninRequestDto {
+    private String username;
+    private String password;
+}
