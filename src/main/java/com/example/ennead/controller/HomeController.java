022cac4dd338e72b6582d526741fb85084e9b564
SeungBum Baek <wer06099@naver.com>
2023-07-05 13:39:40 +0900

    로그인 및 회원가입 기본 틀 작업

---
+++ src/main/java/com/example/ennead/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.ennead.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+public class HomeController {
+    @GetMapping("/home")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "signin";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+}
