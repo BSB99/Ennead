764b68736a139e8e7fda548f189012615b2b8da1
leegyeongsik <gyeongsik1012@gmail.com>
2023-07-06 14:04:11 +0900

    게시판 html

--- src/main/java/com/example/ennead/controller/HomeController.java
+++ src/main/java/com/example/ennead/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.example.ennead.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,5 +26,9 @@ public class HomeController {
     @GetMapping("/board")
     public String board() {
         return "boardInfo";
+    }
+    @GetMapping("/categoryboard")
+    public String categoryboard(){
+        return "board";
     }
 }
