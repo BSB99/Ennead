9df49afc000591d3bd351ae0c03a4d00e845b298
Minji-Sohn2 <cyzh1311@gmail.com>
2023-07-06 20:10:03 +0900

    HomeController에 createboard url 추가, board.html의 등록 버튼 createboard로 연결, 게시글 작성 구현(카테고리 존재하지 않는다고 뜨는 오류 O)

--- src/main/java/com/example/ennead/controller/HomeController.java
+++ src/main/java/com/example/ennead/controller/HomeController.java
@@ -35,4 +35,9 @@ public class HomeController {
     public String categoryboard(){
         return "board";
     }
+
+    @GetMapping("/createboard")
+    public String createBoard() {
+        return "createboard";
+    }
 }
